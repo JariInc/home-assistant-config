5ba8eadd7f4c97aaea23f8e7ba11d43f36cc1046
Jari Ylimäinen <jari@ylimainen.fi>
2019-11-28 20:55:04 +0200

    Fix dividion error

--- mqtt-influxdb/util.py
+++ mqtt-influxdb/util.py
@@ -1,4 +1,5 @@
 from math import log10
+import logging
 
 class Util(object):
 	# Constants
@@ -11,13 +12,18 @@ class Util(object):
 		'Celsius2Kelvin': 273.15,
 	}
 
+	def __init__(self):
+		self.logger = logging.getLogger('mqtt-influxdb.util')
+
 	def waterVapourSaturationPressure(self, temperature, relative_humidity):
 		# https://www.vaisala.com/sites/default/files/documents/Humidity_Conversion_Formulas_B210973EN-F.pdf
 		# Formula 6
 		try:
 			P_ws = self.humidity_constants['A'] * pow(10, (self.humidity_constants['m'] * temperature) / (temperature + self.humidity_constants['T_n']))
 			P_w = P_ws * relative_humidity / 100
-		except ValueError as error:
+		except (ValueError, ZeroDivisionError) as error:
+			self.logger.error("Error calculating waterVapourSaturationPressure %s", error)
+			self.logger.debug("temperature %g, relative_humidity %g", temperature, relative_humidity)
 			P_w = None
 		finally: 
 			return P_w
@@ -28,7 +34,9 @@ class Util(object):
 		try:
 			P_w = self.waterVapourSaturationPressure(temperature, relative_humidity)
 			T_d = self.humidity_constants['T_n'] / ((self.humidity_constants['m'] / log10(P_w / self.humidity_constants['A'])) - 1) 
-		except ValueError as error:
+		except (ValueError, ZeroDivisionError) as error:
+			self.logger.error("Error calculating dewPoint %s", error)
+			self.logger.debug("temperature %g, relative_humidity %g", temperature, relative_humidity)
 			T_d = None
 		finally: 
 			return T_d
@@ -39,7 +47,9 @@ class Util(object):
 		try:
 			P_w = self.waterVapourSaturationPressure(temperature, relative_humidity)
 			A_calc = self.humidity_constants['C'] * (P_w * 100) / (temperature + 273.15)		
-		except ValueError as error:
+		except (ValueError, ZeroDivisionError) as error:
+			self.logger.error("Error calculating absoluteHumidity %s", error)
+			self.logger.debug("temperature %g, relative_humidity %g", temperature, relative_humidity)
 			A_calc = None
 		finally: 
 			return A_calc
